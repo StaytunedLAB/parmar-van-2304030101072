c5d5c05958cc422798067d5d5a18ef3d32b07648
coddicod-van <2304030101072@silveroakuni.ac.in>
2025-12-08 14:57:06 +0530

    add

---
+++ src/fundamentel/day01js-basics.js
@@ -0,0 +1,5 @@
+console.log ("Hello, world!");
+console.log ("Day 01 - javascrript basics");
+/* basic part-1  java script learning to ./ this Example code to learn for "Hello world"*/ 
+console.log("hii")
+console.log("Welcome to 30 days of javascript")
